c67f8ed9006354082ed5f6b3e76b13634e6fa546
karl <karl@df406022-fe11-11dd-a3ef-1f8437fb274a>
2006-01-05 23:39:51 +0000

    Lead the setproctitle format string with a dash.

--- generic/bsd.c
+++ generic/bsd.c
@@ -13,7 +13,7 @@
  * It is provided "as is" without express or implied warranty.
  *
  *-----------------------------------------------------------------------------
- * $Id: bsd.c,v 1.6 2006-01-05 03:23:55 karl Exp $
+ * $Id: bsd.c,v 1.7 2006-01-05 23:39:51 karl Exp $
  *-----------------------------------------------------------------------------
  */
 
@@ -688,7 +688,7 @@ BSD_SetProcTitleObjCmd (clientData, interp, objc, objv)
     }
 
 #ifdef HAVE_SETPROCTITLE
-    setproctitle ("%s", titleString);
+    setproctitle ("-%s", titleString);
     return TCL_OK;
 #else
     Tcl_SetObjResult (interp, Tcl_NewStringObj ("setproctitle undefined on this architecture", -1));
