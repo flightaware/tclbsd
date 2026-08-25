2ba158444296f7d16f7cf479fb2ba73e213c671b
Peter da Silva <peter.dasilva@flightaware.com>
2026-08-25 14:34:56 +0000

    Add Tcl_Size definition

--- generic/bsd.h
+++ generic/bsd.h
@@ -20,6 +20,16 @@
 
 #include <tcl.h>
 
+/* Tcl_Size compatibility for building with tcl8 and earlier. */
+#ifndef TCL_SIZE_MAX
+# define Tcl_GetSizeIntFromObj Tcl_GetIntFromObj
+# define TCL_SIZE_MAX      INT_MAX
+# ifndef Tcl_Size
+    typedef int Tcl_Size;
+# endif
+# define TCL_SIZE_MODIFIER ""
+#endif
+
 #include <sys/types.h>
 #include <unistd.h>
 #include <time.h>
